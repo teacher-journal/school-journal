fde692c529f6db64f80cd580e9ebe768f00b47c6
teacher-journal <db2724949@gmail.com>
2026-09-23 21:23:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -141,5 +141,10 @@ def save_grade():
     save_data(data)
     return jsonify({"success": True})
 
+import os
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
